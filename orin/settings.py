beb42729b85e7518c4818085a650075dd87f979d
Minhazul Islam <arkohassan118@gmail.com>
2025-11-19 19:58:56 +0600

    some changes

--- orin/settings.py
+++ orin/settings.py
@@ -68,17 +68,11 @@ WSGI_APPLICATION = 'orin.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': config('POSTGRES_HOST'),
-            'NAME': config('DB_NAME'),
-            'USER': config('POSTGRES_USER'),
-            'PORT': config('DB_PORT'),
-            'PASSWORD': config('POSTGRES_PASSWORD'), # Replace with your actual password
-        }
-    
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-
+}
 
 
 # Password validation
